751ca63744e331c8e0c3db922bda7a795df8c594
paul le henaff <paul.lehenaff@aptosolutions.co.uk>
2015-12-15 17:06:16 +0000

    Basic SDL project. Displays an image and closes

--- gccp.bat
+++ gccp.bat
@@ -5,4 +5,4 @@ set "directory=%~dp0%"
 set "newvar=%directory%%file%"
 
 echo compiling C++ using -ansi -pedantic-errors -Wall
-g++ -ansi -pedantic-errors -Wall -o orbit %directory%\source_files\orbit.cpp
+g++ -ansi -pedantic-errors -Wall -o orbit %directory%\source_files\orbit.cpp -lmingw32 -lSDLmain -lSDL
--- source_files/orbit.cpp
+++ source_files/orbit.cpp
@@ -1,10 +1,34 @@
-#include <stdio.h>
-#include <iostream>
-using namespace std;
+#include "SDL/SDL.h"
 
-int main (){
-    printf("Hello World\n");
-    char a; 
-    cin>>a;
+int main (int argc, char* args[]){
+
+	 //The images
+    SDL_Surface* hello = NULL;
+    SDL_Surface* screen = NULL;
+
+    //Start SDL
+    SDL_Init( SDL_INIT_EVERYTHING );
+
+ 	//Set up screen
+    screen = SDL_SetVideoMode( 640, 480, 32, SDL_SWSURFACE );
+
+    //Load image
+    hello = SDL_LoadBMP( "resources/doge.bmp" );
+    
+    //Apply image to screen
+    SDL_BlitSurface( hello, NULL, screen, NULL );
+
+    //Update Screen
+    SDL_Flip( screen );
+
+    //Pause
+    SDL_Delay( 2000 );
+
+    //Free the loaded image
+    SDL_FreeSurface( hello );
+
+    //Quit SDL
+    SDL_Quit();
+
     return 0;
 }
