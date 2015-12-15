96da3dfb1bf44cbf9d4efd406b6896dce48fa729
paul le henaff <paul.lehenaff@aptosolutions.co.uk>
2015-12-15 17:19:11 +0000

    Improved

--- source_files/orbit.cpp
+++ source_files/orbit.cpp
@@ -1,34 +1,88 @@
 #include "SDL/SDL.h"
+#include <string>
+
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 480;
+const int SCREEN_BPP = 32;
+
+SDL_Surface *message = NULL;
+SDL_Surface *background = NULL;
+SDL_Surface *screen = NULL;
+
+// Function is used to optimise an image to the correct display format when the image is loaded. 
+// Otherwise SDL has to convert the images on the fly everytime
+SDL_Surface *load_image( std::string filename ) 
+{
+    SDL_Surface* loadedImage = NULL;
+    SDL_Surface* optimizedImage = NULL;
+    loadedImage = SDL_LoadBMP( filename.c_str() );
+
+    if( loadedImage != NULL )
+    {
+        optimizedImage = SDL_DisplayFormat( loadedImage );
+
+        SDL_FreeSurface( loadedImage );
+    }
+    return optimizedImage;
+}
+
+void apply_surface( int x, int y, SDL_Surface* source, SDL_Surface* destination )
+{
+    SDL_Rect offset;
+    offset.x = x;
+    offset.y = y;
+    SDL_BlitSurface( source, NULL, destination, &offset );
+}
 
 int main (int argc, char* args[]){
 
-	 //The images
-    SDL_Surface* hello = NULL;
-    SDL_Surface* screen = NULL;
+	// initialise. if something goes wrong stop everything
+    if( SDL_Init( SDL_INIT_EVERYTHING ) == -1 )
+    {
+        return 1;    
+    }
 
-    //Start SDL
-    SDL_Init( SDL_INIT_EVERYTHING );
+	//Set up the screen
+    screen = SDL_SetVideoMode( SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_BPP, SDL_SWSURFACE );
 
- 	//Set up screen
-    screen = SDL_SetVideoMode( 640, 480, 32, SDL_SWSURFACE );
+    //If there was an error in setting up the screen
+    if( screen == NULL )
+    {
+        return 1;    
+    }
 
-    //Load image
-    hello = SDL_LoadBMP( "resources/doge.bmp" );
+    //Set the window caption
+    SDL_WM_SetCaption( "Orbit", NULL );
     
-    //Apply image to screen
-    SDL_BlitSurface( hello, NULL, screen, NULL );
+    //Load the images
+    message = load_image( "resources/doge.bmp" );
+    background = load_image( "resources/space.bmp" );
 
-    //Update Screen
-    SDL_Flip( screen );
+    //Apply the background to the screen
+    apply_surface( 0, 0, background, screen );
+    apply_surface( 320, 0, background, screen );
+    apply_surface( 0, 240, background, screen );
+    apply_surface( 320, 240, background, screen );
 
-    //Pause
+    //Apply the message to the screen
+    apply_surface( 180, 140, message, screen );
+
+    //Update the screen
+    if( SDL_Flip( screen ) == -1 )
+    {
+        return 1;    
+    }
+
+    //Wait 2 seconds
     SDL_Delay( 2000 );
-
-    //Free the loaded image
-    SDL_FreeSurface( hello );
-
+    
+    //Free the surfaces
+    SDL_FreeSurface( message );
+    SDL_FreeSurface( background );
+    
     //Quit SDL
     SDL_Quit();
-
+    
+    //Return
     return 0;
 }
