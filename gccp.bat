d658beace9b87dc38552598c98440a1a4756dbd7
paul le henaff <paul.lehenaff@aptosolutions.co.uk>
2015-12-15 09:30:34 +0000

    initial commit

---
+++ gccp.bat
@@ -0,0 +1,8 @@
+@echo off
+
+set "file=%~1%" 
+set "directory=%~dp0%" 
+set "newvar=%directory%%file%"
+
+echo compiling C++ using -ansi -pedantic-errors -Wall
+g++ -ansi -pedantic-errors -Wall -o orbit %directory%\source_files\orbit.cpp
